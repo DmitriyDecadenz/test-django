7550f8273e29aaeea6df206e6177deb7979754ca
DmitriyDecadenz <dima.hubul@icloud.com>
2024-06-01 17:25:59 +0400

    add tg-bot

---
+++ src/tg_bot/handlers.py
@@ -0,0 +1,41 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+import src.database.requests as rq
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+import src.keyboards as kb
+
+router = Router()
+
+
+class Reg(StatesGroup):
+    description = State()
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message) -> None:
+    await rq.set_user(message.from_user.id)
+    await message.answer('Добро пожаловать в трекер задач \nдобавить задачу </add> \nпосмотреть задачи </tsk>')
+    await message.answer('Выберите действие', reply_markup=kb.main)
+
+
+@router.message(F.text == '/tsk')
+async def get_task(message: Message) -> None:
+    for task in await rq.get_tasks():
+        await message.answer(f'Задача: \n \n{task.id}. {task.description}')
+
+
+@router.message(F.text == '/add')
+async def add_task(message: Message, state: FSMContext) -> None:
+    await state.set_state(Reg.description)
+    await message.answer('Введите задачу')
+
+
+@router.message(Reg.description)
+async def add_tasks_two(message: Message, state: FSMContext) -> None:
+    await state.update_data(description=message.text)
+    await message.answer('Задача добавлена')
+    data = await state.get_data()
+    await rq.add_task(data["description"])
+    await state.clear()
---
+++ src/tg_bot/main.py
@@ -0,0 +1,23 @@
+import os
+from dotenv import load_dotenv
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from handlers import router
+
+
+async def main() -> None:
+    """Program entry point"""
+    load_dotenv()
+    bot = Bot(token=os.getenv('BOT_TOKEN'))
+    dp = Dispatcher()
+    dp.include_router(router)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен')
