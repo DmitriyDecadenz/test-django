3c72b19a368c886ca9e19e87e37910ab81aa23a7
DmitriyDecadenz <dima.hubul@icloud.com>
2024-06-02 22:53:28 +0400

    fix fetching data

--- tg_bot/fetch_data.py
+++ tg_bot/fetch_data.py
@@ -7,6 +7,7 @@ class ParseData:
         self.url: str = 'http://127.0.0.1:8000/api'
 
     async def get_transaction_json_list(self) -> list[dict] | None:
+        """fetch all transactions"""
         response = httpx.get(f'{self.url}/transactions/?format=json')
         if response.status_code == 204:
             return None
@@ -17,6 +18,7 @@ class ParseData:
         return items_info
 
     async def get_transaction_by_id_json_list(self, transaction_id: int) -> dict | None:
+        """fetch transaction by id"""
         response = httpx.get(f'{self.url}/transaction/{transaction_id}/?format=json')
         if response.status_code == 204:
             return None
@@ -24,10 +26,10 @@ class ParseData:
             items_info = response.json()
         except JSONDecodeError:
             return None
-        print(items_info)
         return items_info
 
     async def get_users_transactions_json_list(self, user_id: int) -> list[dict] | None:
+        """fetch transactions by user id"""
         response = httpx.get(f'{self.url}/user/transactions/?forman=json&user={user_id}')
         if response.status_code == 204:
             return None
